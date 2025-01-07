39aac4530cfa4fe2ec1778c9d8a475fc83032bd0
여준서 <148769872+duwnstj@users.noreply.github.com>
2025-01-07 09:40:19 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     // 반복문의 시간 복잡도를 구하는 문제
+     // 반복문은 다항식으로 나타내면 f(n)=n 으로 나타낼 수 있다.
+     // n번만큼 반복하기 때문 따라서 최고차항의 차수는 n의 1승 즉 1이다.
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n);
+    System.out.println(1);
+    }
+}
